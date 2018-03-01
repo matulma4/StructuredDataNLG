d82dd0039382e5356131b6f3e4bca2964a232834
matulma4 <matulma4@fel.cvut.cz>
2018-03-01 13:08:07 +0100

    Created function to load infobox data

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -2,6 +2,15 @@
 import os
 
 
+def load_infoboxes():
+    result = []
+    with open(path + "/" + dataset + ".box", encoding="utf-8") as f:
+        for person_box in f:
+            info = [k.split(":") for k in person_box.strip().split("\t")]
+            result.append(dict([(v[0], v[1]) for v in info]))
+    return result
+
+
 def load_sentences():
     counts = [int(n.strip()) for n in open(path + "/" + dataset + ".nb")]
     result = []
@@ -22,5 +31,5 @@ if __name__ == '__main__':
         path = "E:/Martin/PyCharm Projects/StructuredDataNLG/data/" + dataset
     else:
         path = "/data/matulma4/wikipedia-biography-dataset/wikipedia-biography-dataset/" + dataset
-
+    load_infoboxes()
     load_sentences()
