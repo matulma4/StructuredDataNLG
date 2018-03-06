1da5bf0d1d25cb4f1378bd98c248e47a2b538b96
matulma4 <matulma4@fel.cvut.cz>
2018-03-06 11:34:34 +0100

    Added method to remove redundant fields

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,14 +1,38 @@
 # -*- coding: utf-8 -*-
 import os
 
+import pickle
+
 
 def load_infoboxes():
     result = []
     with open(path + "/" + dataset + ".box", encoding="utf-8") as f:
         for person_box in f:
             info = [k.split(":") for k in person_box.strip().split("\t")]
-            result.append(dict([(v[0], v[1]) for v in info]))
-    return result
+            result.append(dict([(v[0], v[1]) for v in info if v[1] != "<none>"]))
+    fields = [list(r.keys()) for r in result]
+    return result, fields
+
+
+def get_keys(fields):
+    if not os.path.exists(path + "/" + dataset + ".counts.pickle"):
+        counts = {}
+        for d in fields:
+            for b in d:
+                # c = "_".join(b.split("_")[:-1])
+                c = b
+                if c in counts.keys():
+                    counts[c] += 1
+                else:
+                    counts[c] = 1
+        pickle.dump(counts, open(path + "/" + dataset + ".counts.pickle", "wb"))
+    else:
+        counts = pickle.load(open(path + "/" + dataset + ".counts.pickle", "rb"))
+    known = []
+    for key in counts.keys():
+        if counts[key] > 100:
+            known.append(key)
+    return known
 
 
 def load_sentences():
@@ -31,5 +55,10 @@ if __name__ == '__main__':
         path = "E:/Martin/PyCharm Projects/StructuredDataNLG/data/" + dataset
     else:
         path = "/data/matulma4/wikipedia-biography-dataset/wikipedia-biography-dataset/" + dataset
-    load_infoboxes()
+    r, f = load_infoboxes()
+    if not os.path.exists(path + "/" + dataset + ".key"):
+        k = sorted(get_keys(f))
+        with open(path + "/" + dataset + ".key", "w") as f:
+            for key in k:
+                f.write(key + "\n")
     load_sentences()
