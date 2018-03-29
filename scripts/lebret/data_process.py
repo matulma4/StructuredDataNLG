27fd510da305db86cf473423a6813cfce6865806
matulma4 <matulma4@fel.cvut.cz>
2018-03-29 14:54:12 +0200

    Improved sample creation, model parameters

--- scripts/lebret/data_process.py
+++ scripts/lebret/data_process.py
@@ -1,3 +1,4 @@
+import pickle
 import re
 from collections import Counter
 
@@ -23,8 +24,8 @@ def local_conditioning(tf, f, sentences):
     for i in range(len(f)):
         ib = f[i]
         sent = sentences[i]
-        s_seq = []
-        e_seq = []
+        s_seq = [[F * l + 1] for _ in range(l)]
+        e_seq = [[F * l + 1] for _ in range(l)]
         for word in sent:
             if word in ib.keys():
                 s_seq.append(list(set([j[1] + j[0] * l for j in ib[word]])))
@@ -37,7 +38,7 @@ def local_conditioning(tf, f, sentences):
         start.append(s_seq)
         end.append(e_seq)
     print("Maximum occurrences of words: " + str(max_len))
-    return np.array(start), np.array(end)
+    return np.array(start), np.array(end), max_len
 
 
 def replace_oov(sentences, vocabulary):
@@ -74,27 +75,48 @@ def get_most_frequent(all_sents, sub_numbers):
     else:
         words = [re.sub("[0-9]{2}", "10", a) for b in all_sents for a in b]
     c = Counter(words)
-    result = [a[0] for a in c.most_common(20002) if a[0] != "<NUMBER>" and a[0] != "<YEAR>"]
+    result = [a[0] for a in c.most_common(V + 2) if a[0] != "<NUMBER>" and a[0] != "<YEAR>"]
     return result
 
 
-def process_infoboxes(unique_keys, dict_list):
+def process_infoboxes(unique_keys, dict_list, encoder):
+    global f_size, w_size, f_len, w_len
     fields = []
     le = LabelEncoder()
     tf = dict(zip(unique_keys, le.fit_transform(unique_keys)))
+    table_fields = []
+    table_words = []
+
+    f_len = 0
+    w_len = 0
     for r in dict_list:
         field = {}
+        table_f = set()
+        table_w = set()
         for key in r:
             s = key.split("_")
             k = "_".join(s[:-1])
             idx = int(s[-1])
+            word = r[key]
             if k in unique_keys:
+                table_f.add(tf[k])
+
                 if r[key] in field:
-                    field[r[key]].append((tf[k], min(l, idx), max(1, l - idx + 1)))
+                    field[word].append((tf[k], min(l, idx), max(1, l - idx + 1)))
                 else:
-                    field[r[key]] = [(tf[k], min(l, idx), max(1, l - idx + 1))]
+                    field[word] = [(tf[k], min(l, idx), max(1, l - idx + 1))]
+            if word in encoder.classes_:
+                table_w.add(word)
+        table_fields.append(list(table_f))
+        table_words.append(encoder.transform(list(table_w)))
+        f_size = max(f_size, max(table_fields[-1]))
+        w_size = max(w_size, max(table_words[-1]))
+        f_len = max(f_len, len(table_fields[-1]))
+        w_len = max(w_len, len(table_words[-1]))
         fields.append(field)
-    return fields, tf
+    print("Maximum fields in table: " + str(f_len))
+    print("Maximum words in table: " + str(w_len))
+    return fields, tf, table_fields, table_words
 
 
 def delexicalize(sentences, tables, vocabulary, keys):
@@ -117,32 +139,56 @@ def delexicalize(sentences, tables, vocabulary, keys):
     return sentences, list(field_names)
 
 
-def create_samples(indices, start, end):
-    samples = []
-    loc_size = 25
+def create_samples(indices, start, end, t_f, t_w):
+    samples_context = []
+    samples_ls = []
+    samples_le = []
+    samples_gf = []
+    samples_gw = []
+    target = []
     for i in range(len(indices)):
         idx = indices[i]
         s = start[i]
         e = end[i]
+        glob_field = np.pad(t_f[i], (0, f_len - len(t_f[i])), mode='constant')
+        glob_word = np.pad(t_w[i], (0, w_len - len(t_w[i])), mode='constant')
         for j in range(l, len(idx)):
-            context = idx[j-l:j]
-            s_context = np.array([np.pad(ss, (0, loc_size - len(ss)), mode='constant') for ss in s[j-l:j]])
-            e_context = np.array([np.pad(ee, (0, loc_size - len(ee)), mode='constant') for ee in e[j-l:j]])
-            samples.append(s_context)
-        return samples
+            context = idx[j - l:j]
+            s_context = np.array([np.pad(ss, (0, max_l - len(ss)), mode='constant') for ss in s[j - l:j]])
+            e_context = np.array([np.pad(ee, (0, max_l - len(ee)), mode='constant') for ee in e[j - l:j]])
+            samples_context.append(context)
+            samples_ls.append(s_context)
+            samples_le.append(e_context)
+            samples_gf.append(glob_field)
+            samples_gw.append(glob_word)
+            t = np.zeros(len(output) + 1)
+            try:
+                xx = np.where(output == sentences[i][j])
+                t[xx[0][0]] = 1.0
+            except IndexError:
+                t[-1] = 1.0
+            target.append(t)
+        return np.array(samples_context), np.array(samples_ls), np.array(samples_le), np.array(samples_gf), np.array(
+            samples_gw), np.array(target)
 
 
 if __name__ == '__main__':
     dicts, u_keys = load_infoboxes(path, dataset)
-    f, tf = process_infoboxes(u_keys, dicts)
     sentences = load_sentences()
+    f_size = 0
+    w_size = 0
+    f_len = 0
+    w_len = 0
     indices, encoder, max_idx = get_words(sentences)
+    f, tf, t_fields, t_words = process_infoboxes(u_keys, dicts, encoder)
     print("Size of vocabulary: " + str(max_idx))
-    print("Number of fields: " + str(len(tf)))
-    start, end = local_conditioning(tf, f, sentences)
-    sentences = delexicalize(sentences, dicts, encoder.classes_, u_keys)
-    #
-    create_samples(indices, start, end)
-    #
-    # glob_f_vec = np.arange(len(tf))
-    glob_w_vec = np.arange(max_idx)
+    start, end, max_l = local_conditioning(tf, f, sentences)
+    print("Number of fields: " + str(max_l))
+    sentences, f_names = delexicalize(sentences, dicts, encoder.classes_, u_keys)
+    output = np.concatenate((encoder.classes_, f_names))
+    with open("params.txt", "w") as f:
+        f.write(" ".join([str(max_idx), str(len(tf)*l+2), str(f_size), str(w_size), str(max_l), str(f_len), str(w_len)]))
+    samples_context, samples_ls, samples_le, samples_gf, samples_gw, target = create_samples(indices, start, end,
+                                                                                             t_fields,
+                                                                                             t_words)
+    pickle.dump((samples_context, samples_ls, samples_le, samples_gf, samples_gw, target), open("samples.pickle", "wb"))
--- scripts/lebret/main.py
+++ scripts/lebret/main.py
@@ -1,56 +1,76 @@
+import pickle
+
 import keras.backend as K
-from keras.layers import Embedding, Input, Dense, Activation, Lambda, concatenate, dot, add, Flatten
+from keras.layers import Embedding, Input, Dense, Lambda, concatenate, Flatten
 from keras.models import Model
 
 from config import *
 
 
-def create_model(loc_dim, glob_dim):
+# TODO get actual sizes: vocabulary (V)
+# TODO Maximum fields in table (glob_field_dim)
+# TODO Maximum words in table (glob_word_dim)
+# TODO Maximum occurrences of words in field (loc_dim)
+# TODO Maximum index of local embeddings (max_loc_idx)
+def create_model(loc_dim, glob_field_dim, glob_word_dim, max_loc_idx, max_glob_field_idx, max_glob_word_idx):
     in_len = 10
     cond_len = 10
-    c_input = Input(shape=(in_len,))
+    c_input = Input(shape=(in_len,), name='c_input')
 
-    ls_input = Input(shape=(cond_len,))
-    le_input = Input(shape=(cond_len,))
+    ls_input = Input(shape=(cond_len, loc_dim), name='ls_input')
+    le_input = Input(shape=(cond_len, loc_dim), name='le_input')
 
-    gf_input = Input(shape=(in_len,))
-    gv_input = Input(shape=(in_len,))
+    gf_input = Input(shape=(glob_field_dim,), name='gf_input')
+    gv_input = Input(shape=(glob_word_dim,), name='gw_input')
 
-    mix_input = Input(shape=(V,))
-
-    con_size = n - 1
+    mix_input = Input(shape=(V,), name='mix_input')
 
     context = Embedding(input_dim=V, output_dim=d, input_length=1)(c_input)
     flat_context = Flatten()(context)
 
     # loc_dim: number of fields x number of positions
-    local_start = Embedding(input_dim=loc_dim, output_dim=d, input_length=l, mask_zero=True)(ls_input)
-    local_end = Embedding(input_dim=loc_dim, output_dim=d, input_length=l, mask_zero=True)(le_input)
-    ls_lambda = Lambda(lambda x: K.max(x, axis=1))(local_start)
-    le_lambda = Lambda(lambda x: K.max(x, axis=1))(local_end)
-    global_field = Embedding(input_dim=glob_dim, output_dim=g, input_length=l)(gf_input)
-    global_value = Embedding(input_dim=glob_dim, output_dim=g, input_length=l)(gv_input)
+    local_start = Embedding(input_dim=max_loc_idx, output_dim=d, input_length=l, mask_zero=True)(ls_input)
+    local_end = Embedding(input_dim=max_loc_idx, output_dim=d, input_length=l, mask_zero=True)(le_input)
+    ls_lambda = Lambda(lambda x: K.max(x, axis=2))(local_start)
+    le_lambda = Lambda(lambda x: K.max(x, axis=2))(local_end)
+    flat_ls = Flatten()(ls_lambda)
+    flat_le = Flatten()(le_lambda)
+    global_field = Embedding(input_dim=max_glob_field_idx, output_dim=g, input_length=l)(gf_input)
+    global_value = Embedding(input_dim=max_glob_word_idx, output_dim=g, input_length=l)(gv_input)
     gf_lambda = Lambda(lambda x: K.max(x, axis=1))(global_field)
     gv_lambda = Lambda(lambda x: K.max(x, axis=1))(global_value)
 
-    merged = concatenate([flat_context, ls_lambda, le_lambda, gf_lambda, gv_lambda])
+    merged = concatenate([flat_context, flat_ls, flat_le, gf_lambda, gv_lambda])
 
-    first = Dense(units=nhu, activation='tanh')(merged)  # h(x), 256
-    second = Dense(units=V)(first)  # 20000
+    first = Dense(units=nhu, activation='tanh', input_dim=2176)(merged)  # h(x), 256
+    second = Dense(units=V, name='second')(first)  # 20000
 
     # Mixing outputs
-    mix = Embedding(input_dim=loc_dim, output_dim=d, input_length=V)(mix_input)  # 20000 x  x d
-    third = Dense(units=nhu, activation='tanh')(mix)
-    max_ftr = Lambda(lambda x: K.max(x, axis=1))(third)
-    dot_prod = dot([max_ftr, first], axes=1)
-    final = add([second, dot_prod])
-    activate = Activation('softmax')(final)
+    # mix = Embedding(input_dim=loc_dim, output_dim=d, input_length=V)(mix_input)  # 20000 x  x d
+    # third = Dense(units=nhu, activation='tanh')(mix)
+    # max_ftr = Lambda(lambda x: K.max(x, axis=1))(third)
+    # dot_prod = dot([max_ftr, first], axes=1)
+    # final = add([second, dot_prod])
+    # activate = Activation('softmax')(final)
 
-    model = Model(inputs=[c_input, ls_input, le_input, gf_input, gv_input, mix_input], outputs=activate)
+    model = Model(inputs=[c_input, ls_input, le_input, gf_input, gv_input], outputs=second)
     model.compile(optimizer='sgd', loss='binary_crossentropy')
     return model
 
 
 if __name__ == '__main__':
-    # embed_table()
-    create_model(100, 100)
+    global V
+    with open("params.txt") as f:
+        V, max_loc_idx, glob_field_dim, glob_word_dim, loc_dim, f_len, w_len = [int(a) for a in f.read().split()]
+
+    samples_context, samples_ls, samples_le, samples_gf, samples_gw, target = pickle.load(open("samples.pickle", "rb"))
+    V = target.shape[1]
+    model = create_model(loc_dim, f_len, w_len, max_loc_idx, glob_field_dim, glob_word_dim)
+    # samples_context = np.array(samples_context)
+    # samples_ls = np.array(samples_ls)
+    # samples_le = np.array(samples_le)
+    # samples_gf = np.array(samples_gf)
+    # samples_gw = np.array(samples_gw)
+    model.fit({'c_input': samples_context, 'ls_input': samples_ls, 'le_input': samples_le,
+               'gf_input': samples_gf,
+               'gw_input': samples_gw}, {'second': target}, batch_size=32)
