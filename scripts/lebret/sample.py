f427b8124ae922ea497a946c6be7dcd1b19d286d
matulma4 <matulma4@fel.cvut.cz>
2018-04-05 11:28:39 +0200

    Created class for beam element; Loading expanded

---
+++ scripts/lebret/sample.py
@@ -0,0 +1,4 @@
+class Sample:
+    def __init__(self):
+        self.sentence = []
+        self.score = 0.0
--- scripts/lebret/testing.py
+++ scripts/lebret/testing.py
@@ -10,8 +10,14 @@ sys.path.insert(0, parentdir)
 from config import *
 
 
-def load_classes():
+def load_from_file():
     path_to_files = path + "pickle/" + dataset
     output = pickle.load(open(path_to_files + "/output.pickle", "rb"))
+    t_fields = pickle.load(open(path_to_files + "/t_fields.pickle", "rb"))
+    t_words = pickle.load(open(path_to_files + "/t_words.pickle", "rb"))
     infoboxes = pickle.load(open(path_to_files + "/infoboxes.pickle", "rb"))
-    return output, infoboxes
+    return t_fields, t_words, infoboxes, output
+
+
+if __name__ == '__main__':
+    pass
