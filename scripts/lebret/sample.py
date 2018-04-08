cf86aac210044ef3529b5523bbb5f43880a9d2a3
matulma4 <matulma4@fel.cvut.cz>
2018-04-08 12:31:20 +0200

    Indexes of words are stored

--- scripts/lebret/sample.py
+++ scripts/lebret/sample.py
@@ -1,4 +1,8 @@
 class Sample:
-    def __init__(self):
-        self.sentence = []
-        self.score = 0.0
+    def __init__(self, score, sentence, indexes, t_words):
+        self.sentence = sentence
+        self.score = score
+        try:
+            self.indexes = indexes + [t_words[sentence[-1]]]
+        except KeyError:
+            self.indexes = indexes + [len(t_words.keys())]
